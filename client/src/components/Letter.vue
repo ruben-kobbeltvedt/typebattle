e3f54a693394a72b8c93d907a994a072d0c7e60a
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2025-04-09 16:29:50 +0200

    Added design for game

--- client/src/components/Letter.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const props = defineProps<{
-  letter: string
-}>()
-</script>
-
-<template>
-  <span class="text-gray-800 font-mono">{{ props.letter }}</span>
-</template>
