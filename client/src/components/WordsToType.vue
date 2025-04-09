44362a9c5a38ba4e9fd16bc106b24374fd76ec5d
Ruben Kobbeltvedt <ruben@unimicro.no>
2025-04-09 12:51:55 +0200

    Add Tailwind CSS integration and new components for word typing game

---
+++ client/src/components/WordsToType.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  words: string[]
+}>()
+
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <button
+      @click="shuffleWords"
+      class="mb-4 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+    >
+      Shuffle Words
+    </button>
+  </div>
+
+  <div class="w-[64rem] h-[32rem] bg-gray-100 p-4 overflow-auto rounded-2xl shadow-lg">
+    <div class="flex flex-wrap space-x-4 space-y-2">
+      <span v-for="(word, index) in props.words" :key="index" class="text-gray-800 font-mono">
+        {{ word }}
+      </span>
+    </div>
+  </div>
+</template>
