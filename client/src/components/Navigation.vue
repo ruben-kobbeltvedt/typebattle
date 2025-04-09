4c604c50774d42587665dc62adca55a436b7b3f9
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2025-04-09 21:20:57 +0200

    Added navigation to leaderboard view

---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex justify-end items-center gap-3">
+        <UButton size='xl' icon="i-mdi-trophy-variant" to="/leaderboard" class="bg-[#2C2C2C] text-white border border-[#606060]" />
+        <UButton size='xl' icon="i-mdi-account" to="/user" class="bg-[#2C2C2C] text-white border border-[#606060]" />
+    </div>
+    <!-- <UNavigationMenu :items="items" class="justify-end" /> -->
+</template>
+
+<!-- <script setup lang="ts">
+import { ref } from 'vue'
+
+const items = ref<any[]>([
+    {
+        icon: 'i-mdi-trophy-variant',
+        to: '/leaderboard',
+        type: 'link',
+    },
+    {
+        icon: 'i-mdi-account',
+        to: '/user',
+        type: 'link',
+    }
+]);
+</script> -->
--- client/src/views/Game/Game.vue
+++ client/src/views/Game/Game.vue
@@ -1,18 +1,30 @@
 <template>
-    <div class="bg-[#2C2C2C] w-screen h-screen flex items-center justify-center">
-
-        <div class="flex flex-col justify-start gap-11 p-32">
-            <GameCountdown :seconds="30" />
-            <div>
-                <GameLetter :letter="letter" :written="written" v-for="(letter, index) in letters" :key="index" />
+	<div class="bg-[#2C2C2C] flex flex-col w-screen h-screen">
+        <Navigation class="px-32 pt-8" />
+        <div class="flex-1 flex items-center justify-center">
+            <div class="flex flex-col justify-start gap-11 p-32">
+                <GameCountdown :seconds="30" />
+                <div>
+                    <GameLetter
+                        :letter="letter"
+                        :written="written"
+                        v-for="(letter, index) in letters"
+                        :key="index"
+                    />
+                </div>
             </div>
         </div>
-    </div>
+	</div>
 </template>
 
 <script setup lang="ts">
-import GameCountdown from './GameCountdown.vue'
-import GameLetter from './GameLetter.vue'
-import { ref, computed } from 'vue'
-const letters = ref<string[]>("Dette en telefon skal kunne hoppe fra sånn er denne reisen med kalkun gjennom flere runder i norge".split(""));
+import Navigation from "../../components/Navigation.vue";
+import GameCountdown from "./GameCountdown.vue";
+import GameLetter from "./GameLetter.vue";
+import { ref, computed } from "vue";
+const letters = ref<string[]>(
+	"Dette en telefon skal kunne hoppe fra sånn er denne reisen med kalkun gjennom flere runder i norge".split(
+		""
+	)
+);
 </script>
