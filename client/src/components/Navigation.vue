abc57ef10fbfb27ec854231b4863c917587bebc1
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2025-04-09 21:35:00 +0200

    Removed comments

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -3,22 +3,4 @@
         <UButton size='xl' icon="i-mdi-trophy-variant" to="/leaderboard" class="bg-[#2C2C2C] text-white border border-[#606060]" />
         <UButton size='xl' icon="i-mdi-account" to="/user" class="bg-[#2C2C2C] text-white border border-[#606060]" />
     </div>
-    <!-- <UNavigationMenu :items="items" class="justify-end" /> -->
 </template>
-
-<!-- <script setup lang="ts">
-import { ref } from 'vue'
-
-const items = ref<any[]>([
-    {
-        icon: 'i-mdi-trophy-variant',
-        to: '/leaderboard',
-        type: 'link',
-    },
-    {
-        icon: 'i-mdi-account',
-        to: '/user',
-        type: 'link',
-    }
-]);
-</script> -->
