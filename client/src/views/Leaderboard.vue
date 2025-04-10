f0a59f4d9bfe3ad6ce845bd5246501fd7f82bc14
Lars Andreas Sørensen Vestbø <larsandreas@unimicro.no>
2025-04-10 16:45:19 +0200

    update table to show profile picture

--- client/src/views/Leaderboard.vue
+++ client/src/views/Leaderboard.vue
@@ -2,7 +2,7 @@
   	<div class="bg-[#2C2C2C] flex flex-col w-screen h-screen">
       <Navigation class="px-32 pt-8" />
       <div class="flex flex-col">
-        <h1 class="text-white text-xl ml-10">Vinnertavlen :)</h1>
+        <h1 class="text-white text-xl ml-10">Vinnertavlen</h1>
         <div class="border-2 border-[#B0B0B0] rounded-md h-full flex mx-10 my-2">
             <UTable  :data="dataWithRanking" :columns="columns" class="flex-1" :ui="{td: 'text-white', th: 'text-white'}" />
         </div>
@@ -11,18 +11,36 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, h, ref } from 'vue';
 import { supabase } from '../supabase';
-import { useAsyncState } from '@vueuse/core';
+import { computedAsync, useAsyncState } from '@vueuse/core';
 
 const columns: TableColumn[] = [
     {
-        header: 'Nr.',
+        header: 'Plassering',
         accessorKey: 'index',
     },
     {
         header: 'Brukernavn',
         accessorKey: 'username',
+        cell: ({ row }) => {
+            const { avatar_url, username } = row.original;
+            const publicUrl = supabase.storage.from('avatars').getPublicUrl(avatar_url);
+            const url = publicUrl.data.publicUrl;
+
+            return h(
+                'div',
+                { class: 'flex items-center gap-2' },
+                [
+                    h('img', {
+                        src: url,
+                        alt: username,
+                        class: 'w-8 h-8 rounded-full',
+                    }),
+                    h('span', { class: 'text-white' }, username),
+                ]
+            );
+        },
     },
     {
         header: "Ord per minutt",
@@ -38,12 +56,17 @@ const data = useAsyncState(async () => await supabase
     .from('profiles')
     .select('id, username, avatar_url, high_score, high_score_updated_at')
     .order('high_score_updated_at, high_score', { ascending: false })
-    .limit(10), [])
+    .limit(10), []);
 
-const dataWithRanking = computed(() => {
-    return data.state.value.data?.map((item, index) => ({
+const dataWithRanking = computedAsync(() => {
+    const x = data.state.value.data;
+    const { data: urls } = supabase.storage
+        .from('avatars')
+        .getPublicUrl(x.map((item) => item.avatar_url));
+
+    return x.map((item, index) => ({
         ...item,
-        index: index + 1,
+        index: `${index + 1}. plass`,
         high_score_updated_at: new Date(item.high_score_updated_at).toLocaleDateString('no-NO', {
             year: 'numeric',
             month: '2-digit',
