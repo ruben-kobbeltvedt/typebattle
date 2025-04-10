941431992e42b43ce3dfbcc0dbf4d4e1c37a5bb9
Ruben Kobbeltvedt <ruben@unimicro.no>
2025-04-10 12:20:31 +0200

    Fix Navigation.vue user button route and update Landing.vue structure for improved layout

--- client/src/views/Landing.vue
+++ client/src/views/Landing.vue
@@ -1,4 +1,8 @@
 <template>
-  <Navigation />
-    This is the landing page of the game.
+  <div class="bg-[#2C2C2C] w-screen h-screen">
+    <Navigation class="px-32 pt-8"/>
+    <div class="flex flex-col items-center justify-center">
+      <h1 class="text-white text-4xl mb-8">Welcome to the Typing Game!</h1>
+    </div>
+  </div>
 </template>
